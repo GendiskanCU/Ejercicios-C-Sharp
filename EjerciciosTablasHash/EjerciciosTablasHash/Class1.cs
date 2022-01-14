6fcb9f580dcb585b754423df459bc8da925dd7a6
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-01-14 13:38:25 +0100

    Ejemplos de HashTable

---
+++ EjerciciosTablasHash/EjerciciosTablasHash/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+public class Ejemplo2HashTable
+{
+    public static void Main()
+    {
+        //Creamos e insertamos datos
+        Hashtable miDiccio = new Hashtable(500);
+
+        miDiccio.Add("byte", "8 bits");
+        miDiccio.Add("kilobyte", "1024 bytes");
+        miDiccio.Add("pc", "personal computer");
+
+        //Mostramos algún dato
+        Console.WriteLine("Número de palabras en el diccionario: {0}",
+            miDiccio.Count);
+        if (miDiccio.ContainsKey("pc"))
+        {
+            Console.WriteLine("Significado de la palabra PC: {0}",
+                miDiccio["pc"]);
+        }
+        else
+            Console.WriteLine("No existe la palabra PC");
+
+        Console.ReadLine();
+    }    
+}
---
+++ EjerciciosTablasHash/EjerciciosTablasHash/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+public class EjemploTablaHash
+{
+    public static void Main()
+    {
+        //Creamos e insertamos datos
+        Hashtable miDiccio = new Hashtable();
+        miDiccio.Add("byte", "8 bits");
+        miDiccio.Add("pc", "personal computer");
+        miDiccio.Add("kilobyte", "1024 bytes");
+
+        //Mostramos algún dato
+        Console.WriteLine("Cantidad de palabras en el diccionario: {0}",
+            miDiccio.Count);
+        try
+        {
+            Console.WriteLine("El significado de PC es: {0}",
+                miDiccio["pc"]);
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("No existe esa palabra");
+        }
+
+        Console.ReadLine();
+    }
+}
