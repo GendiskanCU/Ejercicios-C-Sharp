076a88a7f52dfa7cae7999893d92183b9d541cad
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-01-17 11:37:40 +0100

    Ejemplo de uso de enumeradores en pilas y colas

---
+++ EjerciciosColas/EjerciciosColas/Ej_11_3_2 con enumeradores.cs
@@ -0,0 +1,30 @@
+﻿/*(11.3.2) Crea un programa que pida frases al usuario, hasta que introduzca una 
+frase vacía. En ese momento, mostrará todas las frases que se habían introducido.*/
+using System;
+using System.Collections;
+
+public class EjerciciosColas
+{
+    public static void Main()
+    {
+        Queue colaFrases = new Queue();
+
+        string frase;
+
+        do
+        {
+            Console.Write("Introduce una frase (deja en blanco para finalizar): ");
+            frase = Console.ReadLine();
+            if (frase != "")
+                colaFrases.Enqueue(frase);
+
+        } while (frase != "");
+
+        Console.WriteLine("\nLas frases que has ido itroduciendo son:\n");
+        IEnumerator enumerator = colaFrases.GetEnumerator();
+        while(enumerator.MoveNext())
+        {
+            Console.WriteLine(enumerator.Current);
+        }
+    }
+}
