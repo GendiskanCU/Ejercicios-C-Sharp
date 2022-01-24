8978310e23d6980784299756dae5d4874f880d22
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-01-24 10:35:05 +0100

    Ejemplos de Generics

---
+++ Ejemplo de generics List/Ejemplo de generics List/EjemploDirectionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EjemploDictionary
+{
+    public static void Main()
+    {
+        Dictionary<string, int> dic = new Dictionary<string, int>();
+
+        dic.Add("Uno", 1);
+        dic.Add("Dos", 2);
+        dic.Add("Tres", 3);
+        dic.Add("Cuatro", 4);
+        dic.Add("Cinco", 5);
+
+        //Mostramos algún dato
+        Console.WriteLine("Números en el diccionario: {0}", dic.Count);
+
+        if (dic.ContainsKey("Tres"))
+            Console.WriteLine("El número Tres se escribe: {0}", dic["Tres"]);
+    }
+}
---
+++ Ejemplo de generics List/Ejemplo de generics List/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EjemploList
+{
+    public static void Main()
+    {
+        List<string> miLista = new List<string>();
+
+        //Añadimos en orden
+        miLista.Add("Hola,");
+        miLista.Add("soy");
+        miLista.Add("yo");
+
+        //Mostramos lo que contiene
+        Console.WriteLine("Contenido actual:");
+        foreach(string mi in miLista)
+            Console.WriteLine(mi);
+
+        //Accedemos a una posición
+        Console.WriteLine("La segunda palabra es: {0}", miLista[1]);
+
+        //Insertamos en la segunda posición
+        miLista.Insert(1, "Cómo estás?");
+
+        //Mostramos de otra forma lo que contiene
+        for(int i = 0; i < miLista.Count; i++)
+        {
+            Console.WriteLine(miLista[i]);
+        }
+
+        //Buscamos un elemento
+        Console.WriteLine("La palabra \"yo\" está en la posición: {0}",
+            miLista.IndexOf("yo"));
+
+        //Ordenamos
+        miLista.Sort();
+
+        //Mostramos lo que contiene
+        Console.WriteLine("Contenido ordenado:");
+        foreach(string frase in miLista)
+            Console.WriteLine(frase);
+
+        //Buscamos con búsqueda binaria
+        Console.WriteLine("Ahora la palabra \"yo\" está en la posición: {0}" +
+            miLista.BinarySearch("yo"));
+
+        //Invertimos la lista
+        miLista.Reverse();
+
+        //Borramos el segundo dato y la palabra "yo"
+        miLista.RemoveAt(1);
+        miLista.Remove("yo");
+
+        //Mostramos nuevamente lo que contiene
+        Console.WriteLine("Contenido después de darle la vuelta y eliminar dos");
+        foreach (string frase in miLista)
+            Console.WriteLine(frase);
+
+        //Ordenamos y vemos dónde iría un nuevo dato
+        miLista.Sort();
+        Console.WriteLine("La frase \"Hasta luego\" ");
+        int posicion = miLista.BinarySearch("Hasta luego");
+        if (posicion >= 0)
+            Console.WriteLine("Está en la posición {0}", posicion);
+        else
+            Console.WriteLine("No está. Iría en la posición {0}",
+                ~posicion);
+    }
+}
