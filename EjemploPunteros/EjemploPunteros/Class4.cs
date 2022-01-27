e7d03bc4b2961f700e7fb556718a641a9cf943f6
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-01-27 10:28:54 +0100

    La palabra clave fixed para punteros

---
+++ EjemploPunteros/EjemploPunteros/Class4.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class EjemploFixed
+{
+    public unsafe static void Main()
+    {
+        int[] datos = { 10, 20, 30 };
+
+        Console.WriteLine("Leyendo el segundo dato...");
+        fixed (int* posicionDato = &datos[1])
+        {
+            Console.WriteLine("En posicionDato hay {0}", *posicionDato);
+        }
+
+        Console.WriteLine("Leyendo ahora el primer dato...");
+        fixed (int* posicionDato = &datos[0])
+        {
+            Console.WriteLine("Ahora en posicionDato hay {0}", *posicionDato);
+        }
+    }
+}
---
+++ EjerciciosPunteros/EjerciciosPunteros/Ej11_9_6_1.cs
@@ -0,0 +1,28 @@
+﻿/*(11.9.6.1) Crea una programa que pida al usuario 4 números reales, los almacene 
+usando "stackalloc" y luego los recorra incrementando el puntero a partir de la 
+posición del primer elemento.*/
+
+using System;
+
+public class EjercicioAritmeticaPunteros
+{
+    public unsafe static void Main()
+    {
+        const int n = 4;
+        float* numerosReales = stackalloc float[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Introduce un número real:");
+            numerosReales[i] = Convert.ToSingle(Console.ReadLine());
+        }
+
+        //Recorremos el array utilizando la aritmética de punteros
+        for(int j = 0; j < n; j++)
+        {
+            Console.WriteLine("En la posición de memoria {0} está el número {1}",
+                (long)numerosReales, *numerosReales);
+            numerosReales++;
+        }
+    }
+}
