61cab4fd52dcc97cb70137cf73df0ddc0bfb0542
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-01-27 13:08:12 +0100

    Introducción biblioteca DateTime

---
+++ BibliotecaFechaHora/BibliotecaFechaHora/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+//Hacer una pausa temporal en la ejecución del programa
+public class EjemploThread
+{
+    public static void Main()
+    {
+        DateTime fechaActual = DateTime.Now;
+        Console.WriteLine("Son las {0}:{1}:{2}", fechaActual.Hour
+            ,fechaActual.Minute, fechaActual.Second);
+        Console.WriteLine("Pausamos la ejecución 10 segundos");
+        Thread.Sleep(10000);
+        fechaActual = DateTime.Now;
+        Console.WriteLine("Ahora son las {0}:{1}:{2}", fechaActual.Hour
+            , fechaActual.Minute, fechaActual.Second);
+    }
+}
---
+++ BibliotecaFechaHora/BibliotecaFechaHora/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class EjemploDateTime
+{
+    public static void Main()
+    {
+        DateTime fechaActual = DateTime.Now;
+
+        Console.WriteLine("Hoy es {0} de {1} de {2}",
+            fechaActual.Day, fechaActual.Month, fechaActual.Year);
+
+        DateTime diaSiguiente = fechaActual.AddDays(1);
+
+        Console.WriteLine("Mañana será {0}", diaSiguiente.Day);
+
+        //Diferencia entre dos fechas
+        DateTime fechaNacimiento = new DateTime(1970, 9, 15);
+
+        TimeSpan diferenciaFechas = fechaActual.Subtract(fechaNacimiento);
+
+        Console.WriteLine("Desde mi fecha de nacimiento, el {0}-{1}-{2}",
+            fechaNacimiento.Day, fechaNacimiento.Month, fechaNacimiento.Year);
+        Console.WriteLine("Han transcurrido {0} días", diferenciaFechas.Days);
+        Console.WriteLine("Con decimales serían {0} días", diferenciaFechas.TotalDays);
+        Console.WriteLine("Con más detalle sería: {0}", diferenciaFechas);
+    }
+}
