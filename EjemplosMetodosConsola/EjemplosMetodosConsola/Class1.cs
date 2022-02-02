01259b0f367f505c8447bf338857ab8577ed537a
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-02-02 10:44:17 +0100

    Ejemplos métodos de la consola

---
+++ EjemplosMetodosConsola/EjemplosMetodosConsola/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+public class EjemploPosibilidadesConsola
+{
+    public static void Main()
+    {
+        int posX = 40, posY = 10;
+        string simbolos = "^>v<";
+        byte simboloActual = 0;
+        bool terminado = false;
+
+        do
+        {
+            Console.Clear();
+            Console.SetCursorPosition(posX, posY);
+            Console.Write(simbolos[simboloActual]);
+            Thread.Sleep(500);
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo tecla = Console.ReadKey(true);
+                if (tecla.Key == ConsoleKey.RightArrow)
+                    posX++;
+                if (tecla.Key == ConsoleKey.LeftArrow)
+                    posX--;
+                if (tecla.Key == ConsoleKey.Escape)
+                    terminado = true;
+
+                simboloActual++;
+                if (simboloActual > 3)
+                    simboloActual = 0;
+            }
+        } while (!terminado);
+            
+    }
+}
---
+++ EjemplosMetodosConsola/EjemplosMetodosConsola/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+public class EjemploPosibilidadesConsola
+{
+    public static void Main()
+    {
+        int posX, posY;
+
+        Console.Title = "EJEMPLO DE CONSOLA";
+
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+
+        posY = 10;//Fila 10
+
+        Random r = new Random(DateTime.Now.Millisecond);
+        posX = r.Next(20, 40);//Columna aleatoria entre 20 y 40
+        Console.SetCursorPosition(posX, posY);
+        Console.WriteLine("Bienvenido/a");
+
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.SetCursorPosition(10, 15);
+        Console.Write("Pulsa 1 o 2");
+
+        ConsoleKeyInfo tecla;
+
+        do
+        {
+            tecla = Console.ReadKey(true);
+        } while (tecla.KeyChar != '1' && tecla.KeyChar != '2');
+
+        int maxX = Console.WindowWidth;
+        int maxY = Console.WindowHeight;
+
+        Console.SetCursorPosition(maxX - 50, maxY - 1);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Pulsa una tecla para salir...");
+
+        Console.ReadKey(true);
+    }
+}
