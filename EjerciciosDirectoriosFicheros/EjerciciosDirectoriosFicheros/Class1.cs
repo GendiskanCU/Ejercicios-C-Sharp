9fe3a8b4d83e68b43bd20a5cd334e4bdf69edb4e
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-02-04 11:33:55 +0100

    Primer ejercicio Directorios y Ficheros

---
+++ EjerciciosDirectoriosFicheros/EjerciciosDirectoriosFicheros/Class1.cs
@@ -0,0 +1,16 @@
+﻿/*(12.3.3) Crea un programa que permita "pasear" por la carpeta actual, al estilo del 
+antiguo "Comandante Norton": mostrará la lista de ficheros y subdirectorios de la 
+carpeta actual, y permitirá al usuario moverse hacia arriba o abajo dentro de la 
+lista usando las flechas del cursor. El elemento seleccionado se mostrará en color 
+distinto del resto.*/
+
+using System;
+using System.IO;
+
+public class EjercicioFicheros
+{
+    public static void Main()
+    {
+
+    }
+}
---
+++ EjerciciosDirectoriosFicheros/EjerciciosDirectoriosFicheros/Program.cs
@@ -0,0 +1,51 @@
+﻿/*(12.3.1) Crea un programa que muestre en pantalla el contenido de un fichero de 
+texto, cuyo nombre escoja el usuario. Si el usuario no sabe el nombre, podrá 
+pulsar "Intro" y se le mostrará la lista de ficheros existentes en el directorio actual , 
+para luego volver a preguntarle el nombre del fichero*/
+
+using System;
+using System.IO;
+
+public class EjercicioDirectorios
+{
+    public static void Main()
+    {
+        string[] ficherosDeTexto;
+
+        ficherosDeTexto = Directory.GetFiles(".", "*.txt");
+
+        Console.WriteLine("Archivos de texto en el directorio actual:");
+        foreach(string f in ficherosDeTexto)
+        {
+            Console.WriteLine(f);
+        }
+
+        Console.WriteLine("\nEscribe el nombre del fichero que quieres visualizar");
+
+        string fichero = Console.ReadLine();
+
+        if(!File.Exists(fichero))
+        {
+            Console.WriteLine("El fichero {0} no se ha encontrado", fichero);
+        }
+        else
+        {
+            StreamReader lecturaFichero = new StreamReader(fichero);
+            string lineaLeida;
+
+            Console.WriteLine("Contenido del fichero {0}:\n", fichero);
+
+            lineaLeida = lecturaFichero.ReadLine();
+
+            while(lineaLeida != null)
+            {
+                Console.WriteLine(lineaLeida);
+                lineaLeida=lecturaFichero.ReadLine();
+            }
+
+            lecturaFichero.Close();
+        }
+
+        Console.ReadKey();
+    }
+}
