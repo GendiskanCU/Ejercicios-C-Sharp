bc886d71df569210caae7e38d21d4036e4f63fb8
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-02-24 12:43:33 +0100

    Espacios de nombres y enumeraciones

---
+++ EjemploEspacioNombres/EjemploEspacioNombres/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace ConsolaPersonalizada
+{
+    public class Console
+    {
+        public static void WriteLine(string texto)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine(texto);
+        }
+
+        public static void ReadKey()
+        {
+            System.Console.Write("Pulsa una tecla para continuar...");
+            System.Console.ReadKey();
+        }
+    }
+}
+
+
+public class Prueba
+{
+    public static void Main()
+    {
+        ConsolaPersonalizada.Console.WriteLine("Hola");
+
+        ConsolaPersonalizada.Console.ReadKey();
+    }
+}
---
+++ EjercicioPropiedades/EjercicioPropiedades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Persona
+{
+    private string nombre;
+
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+        set
+        {
+            nombre = value;
+        }
+    }
+}
+
+public class Prueba
+{
+    public static void Main()
+    {
+        Persona persona = new Persona();
+
+        persona.Nombre = "MARÍA";
+
+        Console.WriteLine(persona.Nombre);
+
+        Console.ReadKey();
+    }
+}
