254a45808941f5e07666c651b49e0f4e60093f1c
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-03-18 13:31:38 +0100

    Ejemplos LINQ

---
+++ Ejemplo LINQ/Ejemplo LINQ/Class1.cs
@@ -0,0 +1,22 @@
+﻿//Ejemplo ordenar una lista de cadenas de texto
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class EjemploLinq2
+{
+    public static void Main()
+    {
+        List<string> nombres = new List<string>
+        { "pan", "carne", "queso", "fruta", "verdura"};
+
+        var resultadoOrdenado =
+            from nombre in nombres
+            orderby nombre
+            select nombre;
+
+        foreach (string n in resultadoOrdenado)
+            Console.Write("{0}, ", n);
+        Console.WriteLine();
+    }
+}
---
+++ Ejemplo LINQ/Ejemplo LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿//Ejemplo para obtener los números enteros de un array mayores de 10
+
+using System;
+using System.Linq;
+
+public class EjemploLinq
+{
+    public static void Main()
+    {
+        int[] datos = { 20, 5, 7, 4, 25, 18, 5, 8, 21, 2 };
+
+        var result =
+            from n in datos
+            where n > 10
+            select n;
+
+        foreach (int i in result)
+            Console.Write("{0}, ", i);
+        Console.WriteLine();
+    }
+}
