01f030fc674097fc886176e9267df1f493ff59fe
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-03-24 22:56:29 +0100

    Escenario y detección colisiones

--- PruebaMonogame/Game1.cs
+++ PruebaMonogame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace PruebaMonogame
 {
@@ -11,12 +12,42 @@ namespace PruebaMonogame
 
         //Declara las texturas a utilizar
         private Texture2D serpiente;
+        private Texture2D pared;
 
         //Coordenadas para situar a la serpiente
         private double x, y;
 
+        //Número de filas y columnas que tendrá el escenario
+        private int filas, columnas;
+
+        //Array que representará internamente el escenario (la X representa una pared)
+        string[] escenario =
+            {
+                 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+                 "X                              X",
+                 "X                              X",
+                 "X         X             X      X",
+                 "X         X                    X",
+                 "X                              X",
+                 "X              XXXX            X",
+                 "X                              X",
+                 "X                      XXXXX   X",
+                 "X     XXX                      X",
+                 "X       X                      X",
+                 "X       X                      X",
+                 "X       X             X        X",
+                 "X       XXXXXX        X        X",
+                 "X                     X        X",
+                 "X                              X",
+                 "X                              X",
+                 "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+                 };
+
         //Velocidad de movimiento de la serpiente (aprox. píxeles/segundo)
         private int velocidad = 120;
+
+        //Lista de rectángulos que representará internamente todos los obstáculos presentes en el escenario
+        private List<Rectangle> obstaculos = new List<Rectangle>();
 
         public Game1()
         {
@@ -29,13 +60,27 @@ namespace PruebaMonogame
             _graphics.PreferredBackBufferHeight = 720;
             _graphics.ApplyChanges();
 
+            //Establece el nº de casillas del escenario  según el tamaño de pantalla, siendo cada una de 40 de alto y ancho
+            //Que es el tamaño de las texturas que estamos utilizando
+            filas = 720 / 40;
+            columnas = 1280 / 40;
+
             //Posición inicial de la serpiente
             x = 300;
             y = 200;
 
-           
-            
-        }
+            //Rellena la lista de obstáculos en el escenario
+            for (int fila = 0; fila < filas; fila++)
+            {
+                for (int columna = 0; columna < columnas; columna++)
+                {
+                    if (escenario[fila][columna] == 'X')
+                    {
+                        obstaculos.Add(new Rectangle(columna * 40, fila * 40, 40, 40));
+                    }
+                }
+            }
+        }        
 
         protected override void Initialize()
         {
@@ -50,6 +95,7 @@ namespace PruebaMonogame
 
             //Carga las texturas desde la carpeta Content
             serpiente = Content.Load<Texture2D>("Cuerpo");
+            pared = Content.Load<Texture2D>("Pared");
         }
 
         protected override void Update(GameTime gameTime)
@@ -68,6 +114,15 @@ namespace PruebaMonogame
             if (Keyboard.GetState().IsKeyDown(Keys.Down))
                 y += velocidad * gameTime.ElapsedGameTime.TotalSeconds;
 
+
+            //Comprueba si hay colisión entre la serpiente y algún obstáculo
+            //(Los rectángulos de ambos intersectarán o se solaparán)
+            foreach( Rectangle r in obstaculos)
+            {
+                if (r.Intersects(new Rectangle((int)x, (int)y, 40, 40)))
+                    Exit();
+            }
+
             base.Update(gameTime);
         }
 
@@ -80,6 +135,22 @@ namespace PruebaMonogame
             _spriteBatch.Begin();
 
             //Dibuja las texturas dentro del rectángulo de destino definido
+            //El escenario
+            for(int fila = 0; fila < filas; fila++)
+            {
+                for(int columna = 0; columna < columnas; columna++)
+                {
+                    if(escenario[fila][columna] == 'X')
+                    {
+                        _spriteBatch.Draw(pared,
+                            new Rectangle(columna * 40, fila * 40, 40, 40),
+                            Color.White);
+                    }
+                }
+            }
+
+
+            //La serpiente
             _spriteBatch.Draw(serpiente,
                 new Rectangle((int)x, (int)y, 40, 40),
                 Color.White);
