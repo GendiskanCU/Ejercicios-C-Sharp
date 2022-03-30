9fba8e441513bbddc881130b117c5bf03d067100
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-03-30 13:28:34 +0200

    Pong OpenGL: comprobación victoria y fin juego

--- PongOpenGL/Game1.cs
+++ PongOpenGL/Game1.cs
@@ -13,27 +13,36 @@ namespace PongOpenGL
         private Texture2D pelota, raqueta, pared, lineadivisora;
         private SpriteFont tipoLetra;
 
+        //Para controlar si el juego ya está en marcha
+        private bool juegoIniciado;
+
+        //Texto que aparecerá en el marcardor
+        private string marcador = "       PONG!       ";
+
         //Velocidad de movimiento de la pelota
-        private int velocidad = 250;
+        private int velocidad = 550;
         //Para aplicar la velocidad de la pelota en cada eje
         private int velocidadX, velocidadY;
 
         //Velocidad de movimiento de las raquetas
-        private int velocidadRaqueta = 300;
+        private int velocidadRaqueta = 600;
 
         //Posición de la pelota en cada momento
         private double posicionPelotaX, posicionPelotaY;
         //Guarda la posición de salida de la pelota
         private Vector2 posicionSalida = new Vector2(496, 384);
 
-        //Posición de la porción inferior de cada raqueta con valores iniciales
-        private Vector2 posRaqueta1 = new Vector2(33, 384);
-        private Vector2 posRaqueta2 = new Vector2(959, 384);
+        //Posición de la porción inferior de cada raqueta
+        private Vector2 posRaqueta1;
+        private Vector2 posRaqueta2;
         //Límites en Y para la posición de la porción inferior de las raquetas
         private float limiteSuperiorRaqueta = 161f, limiteInferiorRaqueta = 671f;
 
         //Puntuación de los jugadores
-        private int puntuacion1 = 0, puntuacion2 = 0;
+        private int puntuacion1, puntuacion2;
+
+        //Puntuación que se debe alcanzar para lograr la victoria
+        private int puntuacionVictoria = 5;
 
         //Número de filas y columnas en que se divide internamente el campo de juego
         private int filas = 21, columnas = 32;
@@ -42,25 +51,25 @@ namespace PongOpenGL
         //L-Pared izquierda / R-Pared derecha / V-Línea divisora
         private string[] CampoJuego ={
             "UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
-            "L                              R",
-            "L              V               R",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
+            "1                              2",
+            "1              V               2",
             "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"};
 
         //Listas de rectángulos que representan internamente las paredes del campo de juego
@@ -70,6 +79,13 @@ namespace PongOpenGL
         //Arrays de rectángulos que representan internamente las tres partes de ambas raquetas
         private Rectangle[] raquetaJug1 = new Rectangle[3];
         private Rectangle[] raquetaJug2 = new Rectangle[3];
+
+        //Listas de rectángulos que representan internamente las porterías
+        private List<Rectangle> puertaJug1 = new List<Rectangle>();
+        private List<Rectangle> puertaJug2 = new List<Rectangle>();
+
+        //Variable utilizada para realizar pausas temporales
+        private double tiempoPausa = 0f;
 
         public Game1()
         {
@@ -82,20 +98,13 @@ namespace PongOpenGL
             _graphics.PreferredBackBufferWidth = 1024;
             _graphics.PreferredBackBufferHeight = 768;
             _graphics.ApplyChanges();
-            */
-
-            //Posición inicial de la pelota
-            posicionPelotaX = (int)posicionSalida.X;
-            posicionPelotaY = (int)posicionSalida.Y;
-
-            //Movimiento inicial de la pelota
-            velocidadX = velocidad;
-            velocidadY = velocidad;
+            */                  
 
             //Posición inicial de las raquetas
             ActualizaPosicionRaquetas();
 
             //Rellena las listas que representan internamente las paredes del campo de juego
+            //y las porterías de ambos jugadores
             for (int fila = 0; fila < filas; fila++)
             {
                 for (int columna = 0; columna < columnas; columna++)
@@ -104,16 +113,17 @@ namespace PongOpenGL
                     {
                         paredesHorizontales.Add(new Rectangle(columna * 32, 64 + (fila * 32), 32, 32));
                     }
-                }
-            }
-
-            for (int fila = 0; fila < filas; fila++)
-            {
-                for (int columna = 0; columna < columnas; columna++)
-                {
                     if (CampoJuego[fila][columna] == 'L' || CampoJuego[fila][columna] == 'R')
                     {
                         paredesLaterales.Add(new Rectangle(columna * 32, 64 + (fila * 32), 32, 32));
+                    }
+                    if (CampoJuego[fila][columna] == '1')
+                    {
+                        puertaJug1.Add(new Rectangle(columna * 32, 64 + (fila * 32), 32, 32));
+                    }
+                    if (CampoJuego[fila][columna] == '2')
+                    {
+                        puertaJug2.Add(new Rectangle(columna * 32, 64 + (fila * 32), 32, 32));
                     }
                 }
             }
@@ -148,13 +158,30 @@ namespace PongOpenGL
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            MueveRaquetas(gameTime.ElapsedGameTime.TotalSeconds);
-            CompruebaColisionRaquetas();
-            CompruebaColisionPared();
+            if (juegoIniciado)
+            {
+                MueveRaquetas(gameTime.ElapsedGameTime.TotalSeconds);
+                CompruebaColisionRaquetas();
+                CompruebaColisionPared();
+                CompruebaColisionPuertas();
+                CompruebaVictoria();
+                marcador = puntuacion1.ToString("00") + " - " + puntuacion2.ToString("00");
 
-            //Establece la nueva posición de la pelota
-            posicionPelotaX += velocidadX * gameTime.ElapsedGameTime.TotalSeconds;
-            posicionPelotaY += velocidadY * gameTime.ElapsedGameTime.TotalSeconds;
+                //Establece la nueva posición de la pelota
+                posicionPelotaX += velocidadX * gameTime.ElapsedGameTime.TotalSeconds;
+                posicionPelotaY += velocidadY * gameTime.ElapsedGameTime.TotalSeconds;
+            }
+            else
+            {
+                if (tiempoPausa < 2f)
+                {
+                    tiempoPausa += gameTime.ElapsedGameTime.TotalSeconds;
+                }
+                else
+                {
+                    InicioJuego();
+                }
+            }
 
 
             base.Update(gameTime);
@@ -167,9 +194,15 @@ namespace PongOpenGL
             _spriteBatch.Begin();
 
             //Dibuja el marcador
-            string marcador = puntuacion1.ToString("00") + " - " + puntuacion2.ToString("00");
+            Vector2 posicionMarcador = new Vector2(356f, 0f); ;
+            if(!juegoIniciado)
+            {
+                posicionMarcador = new Vector2(10f, 0f);
+                if(puntuacion1 == puntuacionVictoria || puntuacion2 == puntuacionVictoria)
+                    marcador = (puntuacion1 == puntuacionVictoria) ? "   PLAYER 1 WINS!" : "   PLAYER 2 WINS!";
+            }
             _spriteBatch.DrawString(tipoLetra, marcador,
-                new Vector2(356, 0), Color.Green);
+                posicionMarcador, Color.Green);
 
             //Dibuja el campo de juego
             for (int fila = 0; fila < filas; fila++)
@@ -213,6 +246,41 @@ namespace PongOpenGL
         }
 
         /// <summary>
+        /// Espera la pulsación de una determinada tecla e inicia el juego poniendo
+        /// la pelota en movimiento
+        /// </summary>
+        private void InicioJuego()
+        {            
+            //Movimiento inicial de la pelota
+            velocidadX = 0;
+            velocidadY = 0;
+
+            //Posición inicial de la pelota
+            posicionPelotaX = (int)posicionSalida.X;
+            posicionPelotaY = (int)posicionSalida.Y;
+
+            //Posición inicial de las raquetas
+            posRaqueta1 = new Vector2(33, 384);
+            posRaqueta2 = new Vector2(959, 384);
+
+        marcador = " PRESS 1/2 TO START";            
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D1))//Saca el jugador1
+            {
+                velocidadX = velocidad;
+                juegoIniciado = true;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D2))//Saca el jugador2
+            {
+                velocidadX = -velocidad;
+                juegoIniciado = true;
+            }
+
+            puntuacion1 = 0;
+            puntuacion2 = 0;
+        }
+
+        /// <summary>
         /// Comprueba si la pelota colisiona con alguna pared. En caso afirmativo establece
         /// la próxima dirección de movimiento de la pelota
         /// </summary>
@@ -245,6 +313,54 @@ namespace PongOpenGL
                     }
                 }
             }
+        }
+
+        /// <summary>
+        /// Recorre cada una de la lista de porterías, y si la pelota ha colisionado en ella
+        /// aumenta la puntuación del jugador contrario, y vuelve a sacar la pelota del centro
+        /// </summary>
+        private void CompruebaColisionPuertas()
+        {
+            int contadorColisiones = 0;
+            
+            foreach (Rectangle p in puertaJug1)
+            {
+                if (p.Intersects(new Rectangle((int)posicionPelotaX, (int)posicionPelotaY, 32, 32)))
+                {
+                    contadorColisiones++;
+                    if (contadorColisiones == 1)
+                    {
+                        contadorColisiones++;
+                        puntuacion2++;
+                        SaqueDelCentro();
+                    }
+                }
+            }
+            foreach (Rectangle p in puertaJug2)
+            {
+                if (p.Intersects(new Rectangle((int)posicionPelotaX, (int)posicionPelotaY, 32, 32)))
+                {
+                    contadorColisiones++;
+                    if (contadorColisiones == 1)
+                    {
+                        contadorColisiones++;
+                        puntuacion1++;
+                        SaqueDelCentro();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Coloca la pelota en el centro de la pantalla y la dirige hacia el lado
+        /// contrario que tenía anteriormente
+        /// </summary>
+        private void SaqueDelCentro()
+        {
+            posicionPelotaX = posicionSalida.X;
+            posicionPelotaY = posicionSalida.Y;
+            velocidadX *= -1;
+            velocidadY = 0;
         }
 
         /// <summary>
@@ -298,6 +414,10 @@ namespace PongOpenGL
             }
         }
 
+        /// <summary>
+        /// Comprueba si la pelota colisiona con alguno de los fragmentos de las raquetas
+        /// y en caso afirmativo la hace rebotar en la dirección deseada
+        /// </summary>
         private void CompruebaColisionRaquetas()
         {
             int contadorColisiones = 0;
@@ -330,6 +450,19 @@ namespace PongOpenGL
                     }
                 }                
             }
-        }       
+        }
+        
+        /// <summary>
+        /// Comprueba si algún jugador ha alcanzado la puntuación de victoria,
+        /// y si es así cambia el estado del juego
+        /// </summary>
+        private void CompruebaVictoria()
+        {
+            if(puntuacion1 == puntuacionVictoria || puntuacion2 == puntuacionVictoria)
+            {                
+                tiempoPausa = 0f;
+                juegoIniciado = false;                
+            }
+        }
     }
 }
