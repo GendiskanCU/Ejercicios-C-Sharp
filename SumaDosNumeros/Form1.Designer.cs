44d9dda11dfd9c1a53b67f8baef980dae5108c84
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-04-19 14:09:46 +0200

    Introducción a Windows Forms

---
+++ SumaDosNumeros/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace SumaDosNumeros
+{
+    partial class frmSumador
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.tbNumero1 = new System.Windows.Forms.TextBox();
+            this.tbNumero2 = new System.Windows.Forms.TextBox();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.btCalcular = new System.Windows.Forms.Button();
+            this.tbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Location = new System.Drawing.Point(90, 70);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(107, 20);
+            this.lblNumero1.TabIndex = 0;
+            this.lblNumero1.Text = "Primer número";
+            // 
+            // tbNumero1
+            // 
+            this.tbNumero1.Location = new System.Drawing.Point(212, 63);
+            this.tbNumero1.Name = "tbNumero1";
+            this.tbNumero1.Size = new System.Drawing.Size(125, 27);
+            this.tbNumero1.TabIndex = 1;
+            // 
+            // tbNumero2
+            // 
+            this.tbNumero2.Location = new System.Drawing.Point(212, 111);
+            this.tbNumero2.Name = "tbNumero2";
+            this.tbNumero2.Size = new System.Drawing.Size(125, 27);
+            this.tbNumero2.TabIndex = 2;
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Location = new System.Drawing.Point(74, 118);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(123, 20);
+            this.lblNumero2.TabIndex = 3;
+            this.lblNumero2.Text = "Segundo número";
+            // 
+            // btCalcular
+            // 
+            this.btCalcular.Location = new System.Drawing.Point(83, 154);
+            this.btCalcular.Name = "btCalcular";
+            this.btCalcular.Size = new System.Drawing.Size(114, 43);
+            this.btCalcular.TabIndex = 4;
+            this.btCalcular.Text = "Suma";
+            this.btCalcular.UseVisualStyleBackColor = true;
+            this.btCalcular.Click += new System.EventHandler(this.btCalcular_Click);
+            // 
+            // tbResultado
+            // 
+            this.tbResultado.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tbResultado.Location = new System.Drawing.Point(212, 170);
+            this.tbResultado.Name = "tbResultado";
+            this.tbResultado.ReadOnly = true;
+            this.tbResultado.Size = new System.Drawing.Size(125, 27);
+            this.tbResultado.TabIndex = 5;
+            // 
+            // frmSumador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbResultado);
+            this.Controls.Add(this.btCalcular);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.tbNumero2);
+            this.Controls.Add(this.tbNumero1);
+            this.Controls.Add(this.lblNumero1);
+            this.Name = "frmSumador";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.TextBox tbNumero1;
+        private System.Windows.Forms.TextBox tbNumero2;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.Button btCalcular;
+        private System.Windows.Forms.TextBox tbResultado;
+    }
+}
+
