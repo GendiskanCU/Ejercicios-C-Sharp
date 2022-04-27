4ec2826d3cc673522d670a50b25f5014f37b3d0d
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-04-27 14:12:57 +0200

    Carga del fichero de palabras

--- Ahorcado/Form1.Designer.cs
+++ Ahorcado/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Ahorcado
 {
-    partial class Form1
+    partial class Ahorcado
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,61 @@ namespace Ahorcado
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbTitulo = new System.Windows.Forms.Label();
+            this.lbPalabra = new System.Windows.Forms.Label();
+            this.btComenzar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTitulo
+            // 
+            this.lbTitulo.AutoSize = true;
+            this.lbTitulo.Location = new System.Drawing.Point(399, 31);
+            this.lbTitulo.Name = "lbTitulo";
+            this.lbTitulo.Size = new System.Drawing.Size(90, 20);
+            this.lbTitulo.TabIndex = 0;
+            this.lbTitulo.Text = "El Ahorcado";
+            // 
+            // lbPalabra
+            // 
+            this.lbPalabra.AutoSize = true;
+            this.lbPalabra.Location = new System.Drawing.Point(399, 122);
+            this.lbPalabra.Name = "lbPalabra";
+            this.lbPalabra.Size = new System.Drawing.Size(50, 20);
+            this.lbPalabra.TabIndex = 1;
+            this.lbPalabra.Text = "label1";
+            // 
+            // btComenzar
+            // 
+            this.btComenzar.Location = new System.Drawing.Point(504, 341);
+            this.btComenzar.Name = "btComenzar";
+            this.btComenzar.Size = new System.Drawing.Size(94, 29);
+            this.btComenzar.TabIndex = 2;
+            this.btComenzar.Text = "Comenzar";
+            this.btComenzar.UseVisualStyleBackColor = true;
+            this.btComenzar.Click += new System.EventHandler(this.btComenzar_Click);
+            // 
+            // Ahorcado
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btComenzar);
+            this.Controls.Add(this.lbPalabra);
+            this.Controls.Add(this.lbTitulo);
+            this.MaximizeBox = false;
+            this.Name = "Ahorcado";
+            this.Text = "Ahorcado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbTitulo;
+        private System.Windows.Forms.Label lbPalabra;
+        private System.Windows.Forms.Button btComenzar;
     }
 }
 
--- Ahorcado/Form1.cs
+++ Ahorcado/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,55 @@ using System.Windows.Forms;
 
 namespace Ahorcado
 {
-    public partial class Form1 : Form
+    public partial class Ahorcado : Form
     {
-        public Form1()
+
+        private List<string> listaPalabras;
+
+        private StreamReader ficheroListaPalabras;
+
+        public Ahorcado()
         {
             InitializeComponent();
+
+            try
+            {
+                if(File.Exists("ListaPalabras.txt"))
+                {
+                    ficheroListaPalabras = File.OpenText("ListaPalabras.txt");
+
+                    listaPalabras = new List<string>();
+
+                    int finalFichero = 0;
+
+                    finalFichero = ficheroListaPalabras.Read();
+
+                    while(finalFichero != -1)
+                    {
+                        listaPalabras.Add(ficheroListaPalabras.ReadLine());
+
+                        finalFichero = ficheroListaPalabras.Read();
+                    }                   
+
+                    ficheroListaPalabras.Close();
+
+                    lbPalabra.Text = "Número de palabras cargadas: " + listaPalabras.Count;
+                }
+                else
+                {
+                    lbPalabra.Text = "Error al abrir el fichero. No se ha encontrado";
+                }
+            }
+            catch (Exception error)
+            {
+                lbPalabra.Text = "Error al abrir el fichero: " + error.Message;
+            }
+
         }
 
+        private void btComenzar_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
