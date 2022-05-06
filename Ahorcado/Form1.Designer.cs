213a14ecd445df809f8442da44add593d54a8002
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-05-06 14:24:23 +0200

    Juego del Ahorcado: finalizada la lógica

--- Ahorcado/Form1.Designer.cs
+++ Ahorcado/Form1.Designer.cs
@@ -36,6 +36,12 @@ namespace Ahorcado
             this.btCrearBD = new System.Windows.Forms.Button();
             this.pbBarra1 = new System.Windows.Forms.ProgressBar();
             this.lbCaracteresRestantes = new System.Windows.Forms.Label();
+            this.btComprobarLetra = new System.Windows.Forms.Button();
+            this.lbInformacion = new System.Windows.Forms.Label();
+            this.panelJuegoActivo = new System.Windows.Forms.Panel();
+            this.lbLetrasUtilizadas = new System.Windows.Forms.Label();
+            this.tbLetraJugador = new System.Windows.Forms.MaskedTextBox();
+            this.panelJuegoActivo.SuspendLayout();
             this.SuspendLayout();
             // 
             // lbTitulo
@@ -60,9 +66,8 @@ namespace Ahorcado
             this.lbPalabra.ForeColor = System.Drawing.Color.Yellow;
             this.lbPalabra.Location = new System.Drawing.Point(100, 138);
             this.lbPalabra.Name = "lbPalabra";
-            this.lbPalabra.Size = new System.Drawing.Size(235, 36);
+            this.lbPalabra.Size = new System.Drawing.Size(0, 36);
             this.lbPalabra.TabIndex = 1;
-            this.lbPalabra.Text = "Palabra a acertar";
             this.lbPalabra.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.lbPalabra.UseWaitCursor = true;
             this.lbPalabra.Click += new System.EventHandler(this.lbPalabra_Click);
@@ -70,7 +75,7 @@ namespace Ahorcado
             // btComenzar
             // 
             this.btComenzar.Font = new System.Drawing.Font("Bernard MT Condensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btComenzar.Location = new System.Drawing.Point(33, 370);
+            this.btComenzar.Location = new System.Drawing.Point(33, 377);
             this.btComenzar.Name = "btComenzar";
             this.btComenzar.Size = new System.Drawing.Size(182, 41);
             this.btComenzar.TabIndex = 2;
@@ -80,7 +85,7 @@ namespace Ahorcado
             // 
             // btCrearBD
             // 
-            this.btCrearBD.Location = new System.Drawing.Point(642, 370);
+            this.btCrearBD.Location = new System.Drawing.Point(642, 377);
             this.btCrearBD.Name = "btCrearBD";
             this.btCrearBD.Size = new System.Drawing.Size(131, 41);
             this.btCrearBD.TabIndex = 3;
@@ -90,7 +95,7 @@ namespace Ahorcado
             // 
             // pbBarra1
             // 
-            this.pbBarra1.Location = new System.Drawing.Point(33, 417);
+            this.pbBarra1.Location = new System.Drawing.Point(33, 424);
             this.pbBarra1.Name = "pbBarra1";
             this.pbBarra1.Size = new System.Drawing.Size(740, 10);
             this.pbBarra1.TabIndex = 4;
@@ -105,10 +110,71 @@ namespace Ahorcado
             this.lbCaracteresRestantes.ForeColor = System.Drawing.Color.White;
             this.lbCaracteresRestantes.Location = new System.Drawing.Point(100, 183);
             this.lbCaracteresRestantes.Name = "lbCaracteresRestantes";
-            this.lbCaracteresRestantes.Size = new System.Drawing.Size(135, 20);
+            this.lbCaracteresRestantes.Size = new System.Drawing.Size(0, 20);
             this.lbCaracteresRestantes.TabIndex = 5;
-            this.lbCaracteresRestantes.Text = "Juego del Ahorcado";
             this.lbCaracteresRestantes.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btComprobarLetra
+            // 
+            this.btComprobarLetra.Font = new System.Drawing.Font("Bernard MT Condensed", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btComprobarLetra.Location = new System.Drawing.Point(68, 27);
+            this.btComprobarLetra.Name = "btComprobarLetra";
+            this.btComprobarLetra.Size = new System.Drawing.Size(128, 35);
+            this.btComprobarLetra.TabIndex = 7;
+            this.btComprobarLetra.Text = "Comprobar";
+            this.btComprobarLetra.UseVisualStyleBackColor = true;
+            this.btComprobarLetra.Click += new System.EventHandler(this.btComprobarLetra_Click);
+            // 
+            // lbInformacion
+            // 
+            this.lbInformacion.AutoSize = true;
+            this.lbInformacion.BackColor = System.Drawing.Color.Transparent;
+            this.lbInformacion.Cursor = System.Windows.Forms.Cursors.No;
+            this.lbInformacion.Font = new System.Drawing.Font("Bernard MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbInformacion.ForeColor = System.Drawing.Color.White;
+            this.lbInformacion.Location = new System.Drawing.Point(3, 75);
+            this.lbInformacion.Name = "lbInformacion";
+            this.lbInformacion.Size = new System.Drawing.Size(152, 20);
+            this.lbInformacion.TabIndex = 8;
+            this.lbInformacion.Text = "Número de caracteres";
+            this.lbInformacion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panelJuegoActivo
+            // 
+            this.panelJuegoActivo.BackColor = System.Drawing.Color.Transparent;
+            this.panelJuegoActivo.Controls.Add(this.lbLetrasUtilizadas);
+            this.panelJuegoActivo.Controls.Add(this.tbLetraJugador);
+            this.panelJuegoActivo.Controls.Add(this.lbInformacion);
+            this.panelJuegoActivo.Controls.Add(this.btComprobarLetra);
+            this.panelJuegoActivo.Location = new System.Drawing.Point(100, 244);
+            this.panelJuegoActivo.Name = "panelJuegoActivo";
+            this.panelJuegoActivo.Size = new System.Drawing.Size(673, 127);
+            this.panelJuegoActivo.TabIndex = 9;
+            this.panelJuegoActivo.Visible = false;
+            // 
+            // lbLetrasUtilizadas
+            // 
+            this.lbLetrasUtilizadas.AutoSize = true;
+            this.lbLetrasUtilizadas.BackColor = System.Drawing.Color.Transparent;
+            this.lbLetrasUtilizadas.Font = new System.Drawing.Font("Bernard MT Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbLetrasUtilizadas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lbLetrasUtilizadas.Location = new System.Drawing.Point(3, 111);
+            this.lbLetrasUtilizadas.Name = "lbLetrasUtilizadas";
+            this.lbLetrasUtilizadas.Size = new System.Drawing.Size(47, 20);
+            this.lbLetrasUtilizadas.TabIndex = 10;
+            this.lbLetrasUtilizadas.Text = "label1";
+            // 
+            // tbLetraJugador
+            // 
+            this.tbLetraJugador.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tbLetraJugador.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbLetraJugador.Font = new System.Drawing.Font("Bernard MT Condensed", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbLetraJugador.Location = new System.Drawing.Point(3, 3);
+            this.tbLetraJugador.Mask = ">L";
+            this.tbLetraJugador.Name = "tbLetraJugador";
+            this.tbLetraJugador.Size = new System.Drawing.Size(60, 59);
+            this.tbLetraJugador.TabIndex = 9;
+            this.tbLetraJugador.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Ahorcado
             // 
@@ -117,7 +183,8 @@ namespace Ahorcado
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(805, 432);
+            this.ClientSize = new System.Drawing.Size(805, 441);
+            this.Controls.Add(this.panelJuegoActivo);
             this.Controls.Add(this.lbCaracteresRestantes);
             this.Controls.Add(this.pbBarra1);
             this.Controls.Add(this.btCrearBD);
@@ -131,6 +198,8 @@ namespace Ahorcado
             this.Text = "Juego del Ahorcado";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Ahorcado_FormClosing);
             this.Load += new System.EventHandler(this.Ahorcado_Load);
+            this.panelJuegoActivo.ResumeLayout(false);
+            this.panelJuegoActivo.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,6 +213,11 @@ namespace Ahorcado
         private System.Windows.Forms.Button btCrearBD;
         private System.Windows.Forms.ProgressBar pbBarra1;
         private System.Windows.Forms.Label lbCaracteresRestantes;
+        private System.Windows.Forms.Button btComprobarLetra;
+        private System.Windows.Forms.Label lbInformacion;
+        private System.Windows.Forms.Panel panelJuegoActivo;
+        private System.Windows.Forms.MaskedTextBox tbLetraJugador;
+        private System.Windows.Forms.Label lbLetrasUtilizadas;
     }
 }
 
--- Ahorcado/Form1.cs
+++ Ahorcado/Form1.cs
@@ -22,8 +22,14 @@ namespace Ahorcado
         private int numeroPalabras;//Número de palabras en la base de datos
         private string palabraAdivinar;//Para almacenar la palabra que el jugador debe adivinar
         private string palabraParcial;//Para almacenar la palabra que se irá mostrando parcialmente
-        private int numeroCaracteresAdivinar;//Número de caracteres que tiene la palabra a adivinar
+        private List<char> caracteresPalabraParcial;//Para manipular los caracteres individualmente de la palabra parcial
+        private List<char> letrasUtilizadas;//Para guardar las letras que el jugador ya ha utilizado y evitar que repita
+        
+        private int numeroCaracteresAdivinar;//Número de caracteres que tiene/faltan de la palabra a adivinar
+        private int numeroFallos;//Número de fallos que acumula el jugador
+        private const int numeroMaximoFallos = 7;//Número máximo de fallos que puede acumular el jugador
 
+        private Graphics ventanaGrafica;
         public Ahorcado()
         {
             InitializeComponent();
@@ -35,31 +41,42 @@ namespace Ahorcado
             //CreaLaBaseDeDatos();
 
             //Método desechado finalmente, pues solo se cargará una palabra en cada momento
-            //CargaPalabrasDeBaseDatos();
+            //CargaPalabrasDeBaseDatos();           
 
-            
         }
 
+        //Inicializa todas las variables para comenzar de nuevo el juego de adivinar una palabra
         private void btComenzar_Click(object sender, EventArgs e)
         {
-            //Obtiene el número de palabras que hay en la BD
-            CuentaNumeroPalabras();
-
+            numeroFallos = 0;
             palabraParcial = "";
             palabraAdivinar = ObtienePalabraAleatoria();
+            caracteresPalabraParcial = new List<char>();
+            letrasUtilizadas = new List<char>();
+            tbLetraJugador.Text = "";
+            lbLetrasUtilizadas.Text = "";
 
             if (palabraAdivinar != "")
             {                
                 //Actualiza el número de caracteres que tiene la palabra a adivinar
                 numeroCaracteresAdivinar = palabraAdivinar.Length;
-                for(int i = 1; i <= numeroCaracteresAdivinar; i++)
+
+                for(int i = 0; i < numeroCaracteresAdivinar; i++)
                 {
                     palabraParcial = palabraParcial + "_";
+                    caracteresPalabraParcial.Add('_');
                 }
                 MuestraPalabraParcial();
             }
             else
                 MessageBox.Show("No es posible cargar una nueva palabra");
+
+            lbInformacion.Text = "Introduce una letra y pulsa sobre el botón para comprobar si está en la palabra";
+            panelJuegoActivo.Visible = true;
+
+            DibujaAhorcado();
+
+            tbLetraJugador.Focus();
         }
 
 
@@ -90,7 +107,7 @@ namespace Ahorcado
             //Cierra la conexión con la base de datos
             conexionBD.Close();
 
-            lbPalabra.Text = "En la base de datos hay: " + numeroPalabras + " palabras";
+            //lbPalabra.Text = "En la base de datos hay: " + numeroPalabras + " palabras";
         }
 
         /// <summary>
@@ -249,12 +266,18 @@ namespace Ahorcado
 
         private void Ahorcado_Load(object sender, EventArgs e)
         {
-            //Si la BD ya existe, desactivamos el botón de crearla
+            //Si la BD ya existe, desactiva el botón de crearla
             if (File.Exists("palabras.sqlite"))
             {
                 btCrearBD.Visible = false;
                 btCrearBD.Enabled = false;
+
+                //Y Obtiene el número de palabras que hay en la BD
+                CuentaNumeroPalabras();
             }
+
+            //Inicializa la ventana gráfica para poder dibujar el ahorcado
+            ventanaGrafica = this.CreateGraphics();
         }
 
         private void Ahorcado_FormClosing(object sender, FormClosingEventArgs e)
@@ -264,6 +287,159 @@ namespace Ahorcado
             {
                 e.Cancel = true;
             }                       
+        }
+
+        private void btComprobarLetra_Click(object sender, EventArgs e)
+        {
+            if(tbLetraJugador.Text != "")
+            {
+                CompruebaLetraEnPalabra();
+            }
+        }
+
+        //Comprueba si la letra introducida por el jugador está en la palabra, actualiza las
+        //variables de control del juego en función del resultado
+        private void CompruebaLetraEnPalabra()
+        {
+            char letraJugador = Convert.ToChar(tbLetraJugador.Text);
+
+            if (!LetraYaUtilizada(letraJugador))
+            {
+
+                //Añade la letra introducida a las que ya han sido utilizadas
+                letrasUtilizadas.Add(letraJugador);
+                lbLetrasUtilizadas.Text = lbLetrasUtilizadas.Text + letraJugador.ToString() + "-";
+
+                int totalLetras = palabraAdivinar.Length;
+
+                bool letraEncontrada = false;
+
+                //Busca si la letra está en la palabra
+                for (int i = 0; i < totalLetras; i++)
+                {
+                    if (palabraAdivinar[i] == letraJugador)
+                    {
+                        letraEncontrada = true;
+                        caracteresPalabraParcial[i] = letraJugador;
+                        numeroCaracteresAdivinar--;
+                        ActualizaPalabraParcial();
+                    }
+                }
+
+                tbLetraJugador.Text = "";
+                tbLetraJugador.Focus();
+
+                //Si la letra no estaba, se aumenta el número de fallos y comprueba si se ha perdido
+                if (!letraEncontrada)
+                {
+                    numeroFallos++;
+                    DibujaAhorcado();
+                    CompruebaDerrota();
+                }//En caso contrario, se comprueba si se ha vencido
+                else
+                {
+                    CompruebaVictoria();
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("Ya has usado la letra " + letraJugador +". Utiliza otra",
+                    "Letra repetida", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tbLetraJugador.Text = "";
+                tbLetraJugador.Focus();
+            }
+        }
+
+
+        //Comprueba si una letra ya ha sido utilizada por el jugador
+        private bool LetraYaUtilizada(char letra)
+        {
+            foreach(char c in letrasUtilizadas)
+            {
+                if (c == letra)
+                    return true;
+            }
+
+            return false;
+        }
+        
+        //Actualiza y muestra la palabra parcial
+        private void ActualizaPalabraParcial()
+        {
+            palabraParcial = "";
+
+            for(int i = 0; i < caracteresPalabraParcial.Count; i++)
+            {
+                palabraParcial += caracteresPalabraParcial[i];
+            }
+
+            lbPalabra.Text = palabraParcial;
+            lbCaracteresRestantes.Text = "Caracteres por adivinar: " + numeroCaracteresAdivinar.ToString("00");
+        }
+
+        //Comprueba si el jugador ha acertado todos las letras y en caso afirmativo le felicita
+        //y desactiva el panel del juego hasta que comience una nueva partida
+        private void CompruebaVictoria()
+        {
+            if(numeroCaracteresAdivinar <= 0)
+            {
+                MessageBox.Show("¡Enhorabuena, has adivinado la palabra!", "¡¡¡Victoria!!!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                panelJuegoActivo.Visible = false;
+            }
+        }
+
+        //Comprueba si el jugador ha consumido todos los intentos, en cuyo caso finaliza la partida con derrota
+        private void CompruebaDerrota()
+        {
+            if(numeroFallos >= numeroMaximoFallos)
+            {
+                MessageBox.Show("Lo siento, se acabaron los intentos\n" +
+                    "La palabra oculta era: " + palabraAdivinar, "¡Has perdido...!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                panelJuegoActivo.Visible = false;
+            }
+        }
+
+        private void DibujaAhorcado()
+        {
+            Image imagenAhorcado;
+
+            switch (numeroFallos)
+            {
+                case 0:
+                    imagenAhorcado = new Bitmap("imagen_vacia.png");
+                    break;
+                case 1:
+                    imagenAhorcado = new Bitmap("ahorcado01.png");
+                    break;                    
+                case 2:
+                    imagenAhorcado = new Bitmap("ahorcado02.png");
+                    break;
+                case 3:
+                    imagenAhorcado = new Bitmap("ahorcado03.png");
+                    break;
+                case 4:
+                    imagenAhorcado = new Bitmap("ahorcado04.png");
+                    break;
+                case 5:
+                    imagenAhorcado = new Bitmap("ahorcado05.png");
+                    break;
+                case 6:
+                    imagenAhorcado = new Bitmap("ahorcado06.png");
+                    break;
+                case 7:
+                    imagenAhorcado = new Bitmap("ahorcado07.png");
+                    break;
+                default:
+                    imagenAhorcado = new Bitmap("imagen_vacia.png");
+                    break;
+            }
+
+            if(imagenAhorcado != null)
+                ventanaGrafica.DrawImage(imagenAhorcado, 595, 70, 80, 180);
         }
     }
 }
